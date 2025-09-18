39e80976516339bb1b93af8bd86e55d4fc631b04
unknown <jatinnekela@gmail.com>
2025-09-18 21:51:40 +0530

    removed clerk from backend

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ import express from "express"
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
-import { clerkMiddleware } from '@clerk/express'
-import clerkWebhooks from "./controllers/clerkWebhooks.js";
 import userRouter from "./routes/userRoutes.js";
 import hotelRouter from "./routes/hotelRoutes.js";
 import connectCloudinary from "./configs/cloudinary.js";
@@ -18,11 +16,8 @@ app.use(cors())  // enable cross origin resource sharing
 
 // Middleware
 app.use(express.json())
-app.use(clerkMiddleware())
 
-//  API to listen clerk webhooks
-// app.use("/api/clerk",clerkWebhooks);
-
+// Routes
 app.get('/', (req, res) => res.send("API is working"))
 app.use('/api/user/',userRouter)
 app.use('/api/hotels/',hotelRouter)
